67407dbce59f195fb7c6bdd453bd8761274e956e
nadiachaja <nadiagaja13@gmail.com>
2025-01-22 12:56:02 +0100

    begin js interactie zoekbalk

--- overzichtdetail.js
+++ overzichtdetail.js
@@ -1,19 +1,20 @@
 
 //bewegen van de warning in de automatische scan 
-
-document.addEventListener('DOMContentLoaded', function () {
-    const shake = document.querySelectorAll(".update-site.new-errors");
-    shake.forEach(element => {
-        element.classList.add('shake')
-    });
+const shake = document.querySelectorAll(".update-site.new-errors");
+shake.forEach(element => {
+    element.classList.add('shake')
 });
 
 
 //bewegen van de warning in de pages
-
-document.addEventListener('DOMContentLoaded', function () {
-    const wobble = document.querySelectorAll(".update-site.new-errors-2");
-    wobble.forEach(element => {
-        element.classList.add('wobble')
-    });
+const wobble = document.querySelectorAll(".update-site.new-errors-2");
+wobble.forEach(element => {
+    element.classList.add('wobble')
 });
+
+// Zoek het input element
+// Voeg een 'input' event toe
+  // Als ik input krijg, loop dan door alle .pages elementen
+     // Check of .value van het input element in de .textContent van het .pages element staat
+     // Zo niet, gebruik .hidden = true, anders .hidden = false;
+     // Hier heb je/ik .indexOf voor nodig, of .includes
